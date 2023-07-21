fcd7e4bc911815ff6db66a78cfd8481a54ec28c0
mittal0802 <mittalkeshav41@gmail.com>
2023-07-22 05:03:32 +0530

    adding ad&offer marquee

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,37 +1,39 @@
-import Directory from '../../components/directory/directory.component';
+import AdMarquee from "../../components/ad-marquee/ad-marquee.component";
+import Directory from "../../components/directory/directory.component";
 function Home() {
-
   const categories = [
-  {
-    "id": 1,
-    "title": "Hats",
-    "imageUrl": "https://i.postimg.cc/d0c5RfSB/hats.jpg"
-  },
-  {
-    "id": 2,
-    "title": "Jackets",
-    "imageUrl": "https://i.postimg.cc/Y9p8ZW7L/jackets.jpg"
-  },
-  {
-    "id": 3,
-    "title": "Sneakers",
-    "imageUrl": "https://i.postimg.cc/TPjCtczX/sneakers.jpg"
-  },
-  {
-    "id": 4,
-    "title": "Women",
-    "imageUrl": "https://i.postimg.cc/htdCdnMv/women.jpg"
-  },
-  {
-    "id": 5,
-    "title": "Men",
-    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-  }
-]
-
+    {
+      id: 1,
+      title: "Hats",
+      imageUrl: "https://i.postimg.cc/d0c5RfSB/hats.jpg",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+      imageUrl: "https://i.postimg.cc/Y9p8ZW7L/jackets.jpg",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      imageUrl: "https://i.postimg.cc/TPjCtczX/sneakers.jpg",
+    },
+    {
+      id: 4,
+      title: "Women",
+      imageUrl: "https://i.postimg.cc/htdCdnMv/women.jpg",
+    },
+    {
+      id: 5,
+      title: "Men",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
 
   return (
-     <Directory categories={categories} />
+    <div>
+      <AdMarquee />
+      <Directory categories={categories} />
+    </div>
   );
 }
 
